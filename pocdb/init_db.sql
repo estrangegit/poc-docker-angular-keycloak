4fe7629bd9b6367bae83ad6c25e8af976f03f08e
Estrangin <etienne.estrangin@egis-group.com>
2025-09-16 23:45:10 +0200

    chore: use environment variables to build pocdb container

--- pocdb/init_db.sql
+++
@@ -1,4 +0,0 @@
-create tablespace ts_data_poc owner pocdb location '/database/EXAMPLE/TS_DATA_POC';
-create tablespace ts_indx_poc owner pocdb location '/database/EXAMPLE/TS_INDX_POC';
-
-create schema if not exists poc authorization pocdb;
